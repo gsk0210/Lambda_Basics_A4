ac35af148f38a1e673bf7e7257e65e4f94a6be2b
gsk0210 <77238794+gsk0210@users.noreply.github.com>
2021-03-12 19:34:25 +0530

    Update ProductionGeneratorTest.java

--- src/test/java/com/knoldus/ProductionGeneratorTest.java
+++ src/test/java/com/knoldus/ProductionGeneratorTest.java
@@ -20,19 +20,19 @@ class ProductionGeneratorTest {
 
     @Test
     void createProductionTest3(){
-        //Assertions.assertEquals("Invalid Number",productionGenerator.createProduction('a',7));
+        
         Assertions.assertThrows(NumberFormatException.class,()->productionGenerator.createProduction('a',7));
     }
 
     @Test
     void createProductionTest4(){
-        //Assertions.assertEquals(0,productionGenerator.createProduction(-9,95));
+        
         Assertions.assertThrows(NumberFormatException.class,()->productionGenerator.createProduction(-9,95));
     }
 
     @Test
     void createProductionTest5(){
-        //Assertions.assertEquals(0,productionGenerator.createProduction(93,2));
+        
         Assertions.assertThrows(NumberFormatException.class,()->productionGenerator.createProduction(93,2));
     }
 
